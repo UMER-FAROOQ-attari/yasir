a26fab5eaa3aa5ec5e7dd267a4bd1b6589132ca3
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-08-07 14:19:29 +0500

    clear

---
+++ a web/script.js
@@ -0,0 +1,5 @@
+// If you want to dynamically change the cards or add more functionality, you can do it here.
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Cards are ready!");
+  // You can dynamically fetch and add cards here if needed
+});
